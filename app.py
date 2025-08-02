0df9c72729240c461259c5205bd2f65476412e1e
𝙞𝙭𝙣𝙞𝙭𝙙𝙚𝙫𝙨 <info@ixnix.net>
2025-08-02 15:48:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import discord
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
+
+# .env-Datei laden
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Lade Cogs
 @bot.event
 async def on_ready():
     print(f'Bot logged in as {bot.user}')
@@ -13,4 +17,4 @@ async def on_ready():
         if filename.endswith('.py'):
             await bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run("DEIN_TOKEN")
+bot.run(TOKEN)
